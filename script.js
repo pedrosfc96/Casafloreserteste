39d57911530c4814233946388e161c391a523e9d
pedrosfc96 <pedrosfc96@hotmail.com>
2024-12-23 15:03:31 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.getElementById('hamburger').classList.toggle('active');
+    document.getElementById('nav-links').classList.toggle('active');
+});
